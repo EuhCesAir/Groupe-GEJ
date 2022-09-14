00ab5d2b700ed4aac9270f6d86ade2b395a1bc6d
ECR_ <113091060+EuhCesAir@users.noreply.github.com>
2022-09-14 08:35:19 +0400

    Create accueil.php

---
+++ accueil.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr-FR">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>SériesTV | Acceuil</title>
+    </head>
+    <body>
+        <header>
+            <img src="" alt="Logo" class="logo">
+            <nav>
+                
+            </nav>
+        </header>
+        <footer>
+
+        </footer>
+    </body>
+</html>
