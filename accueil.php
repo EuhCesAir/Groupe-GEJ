e834e5b892a175906b874a84411e1fa1f906856b
ECR_ <113091060+EuhCesAir@users.noreply.github.com>
2022-09-21 11:18:35 +0400

    Update accueil.php

--- accueil.php
+++ accueil.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="styless.css">
         <title>SériesTV | Acceuil</title>
     </head>
     <body>
@@ -21,14 +21,23 @@
             <!-- Faire une barre de navigation -->
         </header>
 
-        <section class="mieux_notees">
+        <section class="mieux-notees">
             <div class="description">
-                <h4>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus, 
-                    voluptates dolores maiores reprehenderit autem saepe. 
-                    Maxime illo ipsum nisi eaque accusantium libero dolore eos numquam enim. 
-                    Error, hic! Blanditiis corrupti cum dolorum enim laboriosam et in incidunt impedit! 
-                    Exercitationem corporis a hic nisi ab? In eos illo aperiam? Distinctio, exercitationem.
+                <h4>
+                    Bienvenue sur <strong>JugezTV</strong>, votre nouveau site préféré de notation de séries !
+                    Vous pourrez y retrouver les séries du moment ou encore les séries de votre enfance.
                 </h4>
+            </div>
+
+            <div class="series">
+                <img src="assets/big_bang_theory.jpg" alt="Big Bang Theory">
+                <img src="assets/the_flash.jpg" alt="Friends">
+                <img src="assets/dexter.jpg" alt="Dexter">
+                <img src="assets/peaky_blinders.jpg" alt="Peaky Blinders">
+                <img src="assets/resident_evil.jpg" alt="Resident Evil">
+                <img src="assets/squid_game.jpg" alt="Squid Game">
+                <img src="assets/teen_wolf.jpg" alt="Teen Wolf">
+                <img src="assets/the_100.jpg" alt="The 100">
             </div>
         </section>
 
